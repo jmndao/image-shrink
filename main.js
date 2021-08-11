bb7bd5c099e19b977548495ad216e1c112867fc5
jmndao <jmndao@gmail.com>
2021-08-11 12:15:49 +0000

    Absolute Icon path

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const createMainWindow = () => {
         title: "Image Shrink",
         width: 500,
         height: 600,
-        icon: './assets/icons/Icon_256x256.png'
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`
     });
 
     mainWindow.loadFile('./app/index.html');
